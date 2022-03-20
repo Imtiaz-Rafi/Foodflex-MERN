641e52859a30c6b2dc7897b4e7ed1c83d66da21b
Imtiaz Rafi <imtiazrafi199918@gmail.com>
2022-03-20 23:41:58 +0600

    deploying in heroku server

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ require("./src/routes/routes")(app);
 require("./src/start/config")();
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV == "production") {
+    // Accessing the Path Module
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, "./client/build")));
+    app.get("*", function (req, res) {
+        res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    });
+}
+
 app.listen(port, () => {
     // console.log("Listening on port",port);
     winston.info(`Server Listening to Port ${port}`);
--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,7 @@ const config = require("config");
 
 module.exports = function () {
     const db = config.get("DB");
-    mongoose
-        .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => winston.info(`Connected to ${db}`));
+    mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+        winston.info(`Connected to Database`);
+    });
 };
