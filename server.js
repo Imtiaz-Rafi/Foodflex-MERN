54c71f920134f450e2b7ded31917340f904df085
Imtiaz Rafi <imtiazrafi199918@gmail.com>
2022-03-22 00:13:53 +0600

    files restacked"

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+// const winston = require("winston");
+const path = require("path");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+require("./start/logger")();
+require("./db/db")();
+require("./routes/routes")(app);
+require("./start/config")();
+
+// app.get("/", (req, res) => {
+//     res.send("This is From server.");
+// });
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server Listening to Port ${port}`);
+    // winston.info(`Server Listening to Port ${port}`);
+});
